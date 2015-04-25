b47046da820b4c80840cea6a2bb522e770f1ea75
avigupta <avinaash.gupta50@gmail.com>
2015-04-24 20:00:32 -0500

    fix

--- app/models/event_image.rb
+++ app/models/event_image.rb
@@ -1,9 +1,9 @@
 class EventImage < ActiveRecord::Base	
 	has_attached_file :image,
 					:storage => :dropbox,
-					:dropbox_credentials => Root.root.join("config/dropbox.yml"),
+					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 					#:dropbox_options => {...},
-					:path => ":events/:id_:filename",
+					#:path => ":events/:id_:filename",
 					styles: {
 					    thumb: '100x100>',
 					    square: '200x200#',
