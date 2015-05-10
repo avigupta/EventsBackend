a242808c8f9037784a0bc7091b4c58c31a1a5ac2
avigupta <avinaash.gupta50@gmail.com>
2015-05-10 15:58:00 -0500

    fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
 	validates :name, presence: true
 
 	belongs_to :user
-	has_many :invitees
-	has_one :event_image
+	has_many :invitees, :dependent => :destroy
+	has_one :event_image, :dependent => :destroy
 end
